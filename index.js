c23357c83fe0f546ec5f4697541680e17d78d9fb
MALLAVALLI MUTYALA SAI SASANK <108589072+sasank-229@users.noreply.github.com>
2023-12-20 01:21:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function linkedin(){
+    Finalresult='<a href="https://www.linkedin.com/in/mutyala-sai-sasank-mallavalli/612272227" target="_blank">https://www.linkedin.com/in/mutyala-sai-sasank-mallavalli/612272227</a>';
+    // Finalresult='https://www.linkedin.com/in/mutyala-sai-sasank-mallavalli/612272227'
+    document.getElementById('answer').innerHTML = Finalresult;
+}
+function email(){
+    Finalresult='sasankmallavalli229@gmail.com'
+    document.getElementById('answer').innerText = Finalresult;
+}
+function phone(){
+    Finalresult='+919121368199'
+    document.getElementById('answer').innerText = Finalresult;
+}
+function github(){
+    Finalresult='<a href="https://github.com/sasank-229" target="_blank">https://github.com/sasank-229</a>'
+    document.getElementById('answer').innerHTML = Finalresult;
+}
+function twitter(){
+    Finalresult='<a href="https://Twitter.com/MMS_Sasank" target="_blank">https://Twitter.com/MMS_Sasank</a>'
+    // Finalresult='https://Twitter.com/MMS_Sasank'
+    document.getElementById('answer').innerHTML = Finalresult;
+}
